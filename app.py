0ce08b297bf246728b178f730211533610fbbd75
gauravchoudhary45 <gauravchoudhary29907@gmail.com>
2026-06-20 17:45:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 # Serverless Production Database Configuration (Writes directly to Vercel's allowed /tmp space)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # --- IMAGE UPLOAD CONFIGURATION ---
